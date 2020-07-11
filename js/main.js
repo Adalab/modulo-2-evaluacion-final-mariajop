9f50a18999cd1f5670dedef90961a6d6c624d54c
mariajop <mariajops@gmail.com>
2020-07-11 19:17:13 +0200

    favorites not working yet - draft local storage and reset

--- js/main.js
+++ js/main.js
@@ -59,21 +59,59 @@ const listenSeries = () => {
 /// 4. AÑADO ESOS ITEMS CLICADOS AL ARRAY DE FAVORITOS:
 
 const gatherClicks = (event) => {
+  debugger;
   const clickedId = parseInt(event.currentTarget.id);
-  const result = results.find((resultItem) => resultItem.id === clickedId);
-  favorites.push(result);
-  console.log(favorites);
+  const favFilm = results.find((result) => result.id === clickedId);
+  favorites.push(clickedId);
 
-  //NO TOMA ID, CREO QUE NO VA A FUNCIONAR
+  addToFavorites();
 
-  //Equivalente a:
-  // for (const result of results) {
-  //   if (result.id === clickedId) {
-  //     console.log(result);
-  //   }
-  // }
+  //NO TOMA ID, tengo que asociar otro item que no es result.id
 };
 
 /// 4. PINTO EL ARRAY EN LA SECCIÓN FAVORITOS:
 
-const addToFavorites 
+const addToFavorites = () => {
+  let insertHTML = '';
+  for (const result of results) {
+    insertHTML += `<li class="list__item" id="${result.show.id}">`;
+    if (result.show.image === null) {
+      insertHTML += `<img src="./images/img-not-available.jpg" class="item__img" alt="cover image not available"/>`;
+    } else {
+      insertHTML += `<img src="${result.show.image.medium}" class="item__img" alt="cover image"/>`;
+    }
+    insertHTML += `<img class="favs__button--delete" title="delete" src="./images/delete_icon.svg">`;
+    insertHTML += `<h3 class="item__title">${result.show.name}</h3>`;
+    insertHTML += `</li>`;
+  }
+  const favItem = document.querySelector('.js-favorites-list');
+  favItem.innerHTML = insertHTML;
+
+  removeFromFavorites();
+
+  //DEFINIR FUNCIÓN QUE SE ACTIVA EN EL EVENT LISTENER DEL BOTÓN PARA ELIMINAR FAVORITOS. PLUS: BOTÓN QUE BORRA TODOS LOS FAVS.
+};
+
+//ALMACENAR FAVORITOS EN LOCAL STORAGE:
+const updateLocalStorage = () => {
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+};
+
+const getFromLocalStorage = () => {
+  const data = JSON.parse(localStorage.getItem('favorites'));
+  if (favorites !== null) {
+    favorites = data;
+  }
+};
+
+// BOTÓN BORRAR TODOS
+
+const btnDeleteAll = document.querySelector('POR CREAR');
+
+const deleteAll = () => {
+  cart = [];
+  updateLocalStorage();
+  showResults();
+};
+
+btnDeleteAll.addEventListener('click', resetCart);
